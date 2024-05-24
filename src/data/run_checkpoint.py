0bd9e4cc556ed4648d0e57f54ffa0d2d64e218e2
jernej10 <jernej.ofic@gmail.com>
2024-05-24 19:27:03 +0200

    Feat: test & validate

---
+++ src/data/run_checkpoint.py
@@ -0,0 +1,20 @@
+import great_expectations as ge
+from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
+import pandas as pd
+
+
+def main():
+    context = ge.get_context()
+    result: CheckpointResult = context.run_checkpoint(checkpoint_name="my_checkpoint")
+
+    if not result["success"]:
+        print("[Validate]: Checkpoint validation failed!")
+    else:
+        print("[Validate]: Checkpoint validation passed!")
+
+    current_data = pd.read_csv("data/current_data.csv")
+    reference_data_path = "data/reference_data.csv"
+    current_data.to_csv(reference_data_path, index=False)
+
+if __name__ == "__main__":
+    main()
